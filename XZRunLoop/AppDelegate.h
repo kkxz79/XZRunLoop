7d6721b9cc4a0f9a4e8b28973ec201fe86163bd6
kkxz79 <kkxz79@126.com>
2018-09-12 17:10:39 +0800

    Initial Commit

---
+++ XZRunLoop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZRunLoop
+//
+//  Created by kkxz on 2018/9/12.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZRunLoop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZRunLoop
+//
+//  Created by kkxz on 2018/9/12.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

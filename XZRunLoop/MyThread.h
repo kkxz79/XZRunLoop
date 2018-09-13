347c8275ba1de74834706c8da3124d82e8163776
kkxz79 <kkxz79@126.com>
2018-09-13 09:56:59 +0800

    首次提交

---
+++ XZRunLoop/MyThread.h
@@ -0,0 +1,13 @@
+//
+//  MyThread.h
+//  XZRunLoop
+//
+//  Created by kkxz on 2018/9/12.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyThread : NSThread
+
+@end
---
+++ XZRunLoop/MyThread.m
@@ -0,0 +1,13 @@
+//
+//  MyThread.m
+//  XZRunLoop
+//
+//  Created by kkxz on 2018/9/12.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import "MyThread.h"
+
+@implementation MyThread
+
+@end
